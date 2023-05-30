2306a2b36b7cdf044fb9d81bf88a9815f7e79247
Luciano Júnior <luciano.vieira@mercadolivre.com>
2023-05-30 14:31:05 -0300

    Update control_without_pid.ino

--- control_without_pid.ino
+++ control_without_pid.ino
@@ -1,4 +1,5 @@
 #include <AFMotor.h>
+#include <PID_v1.h>
 #include "Adafruit_VL53L0X.h"
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
@@ -9,105 +10,114 @@ const int BUTTON2 = A2;
 const int BUTTON3 = A3;
 
 uint16_t checkPoint3Floor = 58;
-uint16_t checkPoint2Floor = 130;
-uint16_t checkPoint1Floor = 250;
-uint16_t checkPointTFloor = 300;
+uint16_t checkPoint2Floor = 140;
+uint16_t checkPoint1Floor = 247;
+uint16_t checkPointTFloor = 310;
+
 uint16_t startTime;
+uint16_t currentTime;
+uint16_t period = 8500;  //8.5 seconds
+
+double setPointPID, entryPointPID, outputPID;
+// Olhometro
+float Kp = 3;
+float Ki = 5;
+float Kd = 2;
+
+// Lugar das raizes
+// float Kp = 142.8216694;
+// float Ki = 14.28221669;
+// float Kd = 14.222169;
+
+// ZN metodo 2
+// float Kp = 8569.300128;
+// float Ki = 92550.62174;
+// float Kd = 1983.587557;
+
+// ZN metodo 2 dividido por kp
+// float Kp = 1;
+// float Ki = 10.800;
+// float Kd = 0.2314760;
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 AF_DCMotor motor(3);
+PID myPID(&entryPointPID, &outputPID, &setPointPID, Kp, Ki, Kd, DIRECT);
 
-void setup()
-{
+void setup() {
   // setup buttons
   pinMode(BUTTONT, INPUT_PULLUP);
   pinMode(BUTTON1, INPUT_PULLUP);
   pinMode(BUTTON2, INPUT_PULLUP);
   pinMode(BUTTON3, INPUT_PULLUP);
   // setup serial monitor
-  Serial.begin(115200);
+  Serial.begin(2400);
   // setup lcd
-  lcd.setBacklight(HIGH);
+  lcd.setBacklight(LOW);
   lcd.begin(16, 2);
   // setup sensor
   lox.begin();
+
+  // pid setup
+  myPID.SetMode(AUTOMATIC);
+
+  //start time
+  startTime = millis();
 }
 
-uint16_t getMeasure()
-{
+uint16_t getMeasure() {
   VL53L0X_RangingMeasurementData_t measure;
   lox.rangingTest(&measure, false);
-  if (measure.RangeStatus != 4)
-  {
+  if (measure.RangeStatus != 4) {
     return measure.RangeMilliMeter;
-  }
-  else
-  {
+  } else {
     return 0;
   }
 }
 
-void displayFloor(int floor)
-{
+void displayFloor(int floor) {
   lcd.setCursor(0, 1);
   lcd.print("Floor: ");
   lcd.print(floor);
 }
 
-void displayMeasure(uint16_t measure)
-{
+void displayMeasure(uint16_t measure) {
   lcd.setCursor(0, 0);
   lcd.print("Distance: ");
   lcd.print(measure);
 }
 
-int checkFloor(uint16_t measure)
-{
-  if (measure >= checkPointTFloor)
-  {
+int checkFloor(uint16_t measure) {
+  if (measure >= checkPointTFloor) {
     return 0;
-  }
-  else if (measure >= checkPoint1Floor)
-  {
+  } else if (measure >= checkPoint1Floor) {
     return 1;
-  }
-  else if (measure >= checkPoint2Floor)
-  {
+  } else if (measure >= checkPoint2Floor) {
     return 2;
-  }
-  else if (measure >= checkPoint3Floor)
-  {
+  } else if (measure >= checkPoint3Floor) {
     return 3;
-  }
-  else
-  {
+  } else {
     return -1;
   }
 }
 
-uint16_t checkInputs()
-{
-  if (digitalRead(BUTTONT) == 0)
-  {
+uint16_t checkInputs() {
+  if (digitalRead(BUTTONT) == 0) {
     Serial.print("Button T pushed, actual floor: ");
     Serial.println(checkFloor(getMeasure()));
     return checkPointTFloor;
   }
-  if (digitalRead(BUTTON1) == 0)
-  {
+  if (digitalRead(BUTTON1) == 0) {
     Serial.print("Button 1 pushed, actual floor: ");
     Serial.println(checkFloor(getMeasure()));
     return checkPoint1Floor;
   }
-  if (digitalRead(BUTTON2) == 0)
-  {
+  if (digitalRead(BUTTON2) == 0) {
     Serial.print("Button 2 pushed, actual floor: ");
     Serial.println(checkFloor(getMeasure()));
     return checkPoint2Floor;
   }
-  if (digitalRead(BUTTON3) == 0)
-  {
+  if (digitalRead(BUTTON3) == 0) {
     Serial.print("Button 3 pushed, actual floor: ");
     Serial.println(checkFloor(getMeasure()));
     return checkPoint3Floor;
@@ -115,83 +125,104 @@ uint16_t checkInputs()
   return 0.0;
 }
 
-void analitics()
-{
-  Serial.print("Distance: ");
+void analitics() {
+  Serial.print("Distance:");
   Serial.print(getMeasure());
-  Serial.print(" Time: ");
-  Serial.println((millis() - startTime) / 1000.0);
+  Serial.print(",");
+  Serial.print("Time:");
+  Serial.print((millis() - startTime) / 1000.0);
+  Serial.print(",");
+  Serial.print("Output:");
+  Serial.println(outputPID);
 }
 
-void moveDown()
-{
-  motor.setSpeed(255); // Define a velocidade maxima
+void moveDown(int speed) {
+  motor.setSpeed(speed);  // Define a velocidade maxima
   motor.run(FORWARD);
 }
 
-void moveUp()
-{
-  motor.setSpeed(255);
+void moveUp(int speed) {
+  motor.setSpeed(speed);
   motor.run(BACKWARD);
 }
 
-void stoped()
-{
+void stoped() {
   motor.setSpeed(0);
   motor.run(RELEASE);
 }
 
-void loop()
-{
+void calculatePID(int input) {
+  entryPointPID = getMeasure();
+  setPointPID = input;
+  myPID.Compute();
+  // Serial.print("Input: ");
+  // Serial.print(entryPointPID);
+  // Serial.print(" setPoint: ");
+  // Serial.print(setPointPID);
+  // Serial.print(" output: ");
+  // Serial.println(outputPID);
+}
+
+void loop() {
 
   uint16_t measure = getMeasure();
 
   lcd.clear();
   displayMeasure(measure);
   displayFloor(checkFloor(measure));
-  analitics();
+  // analitics();
   int input = checkInputs();
 
-  if (input == 0)
-  {
+  if (input == 0) {
     stoped();
-  }
-  else
-  {
+  } else {
     // startTime = millis();
     // while (abs(input - getMeasure()) >= 1)
     // {
-    //   int diff = input - getMeasure();
-    //   Serial.println();
-    //   Serial.print(diff);
-    //   if (diff < 0)
-    //   {
-    //     moveUp();
-    //   }
-    //   else
-    //   {
-    //     moveDown();
-    //   }
+    //     int diff = input - getMeasure();
+
+    //     if (diff < 0)
+    //     {
+    //         analitics();
+    //         calculatePID(input);
+    //         moveUp(255-outputPID);
+    //     }
+    //     else
+    //     {
+    //         analitics();
+    //         calculatePID(input);
+    //         moveDown(outputPID);
+    //     }
     // }
-    if (input > getMeasure())
-    {
-      while (input > getMeasure())
-      {
+
+    startTime = millis();
+    if (input > getMeasure()) {
+      while (input > getMeasure()) {
         analitics();
-        moveDown();
+        calculatePID(input);
+        moveDown(outputPID);
+      }
+      while (millis() - startTime <= period) {
+        stoped();
+        analitics();
       }
       return;
     }
-    if (input < getMeasure())
-    {
-      while (input < getMeasure())
-      {
+    if (input < getMeasure()) {
+      while (input < getMeasure()) {
         analitics();
-        moveUp();
+        calculatePID(input);
+        moveUp(255 - outputPID);
+      }
+      while (millis() - startTime <= period) {
+        stoped();
+        analitics();
       }
       return;
     }
   }
+
+
 
   // if (digitalRead(BUTTONT) == 0) {
   //   Serial.println("Botao T pressionado: ");
